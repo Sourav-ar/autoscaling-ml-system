60a59b62f4b0f73ef467e05a535fc144d0aaed59
Sourav-ar <sourav.choudhary2023@vitstudent.ac.in>
2026-04-15 02:41:40 +0530

    app.py

--- cloud/server/app.py
+++ cloud/server/app.py
@@ -67,5 +67,5 @@ def health():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # IMPORTANT FIX
+    port = int(os.environ.get("PORT", 8080))  # IMPORTANT FIX
     app.run(host='0.0.0.0', port=port)
